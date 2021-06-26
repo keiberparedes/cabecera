2650a8e94ff65936b0980c69c65a70e19559f1ba
keiberparedes <84099590+keiberparedes@users.noreply.github.com>
2021-06-26 10:55:59 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+console.log ("holaaaaaa")
+
+JQuery ('document').ready (funtion ($) {
+
+
+var menuBtn = $ ('.menu-icon').,
+menu= $ ('.navegation ul');
+
+menuBtn.click(function(){ 
+
+if (menu.hasClass('show') ) {
+
+    menu.removeClass('show') ;
+
+} else {
+
+     (menu.addClass('show') ;
+}
+
+    
+    
+    });
+
+});
+
